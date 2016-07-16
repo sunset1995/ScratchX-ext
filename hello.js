de6f3f40d8af3f19974d6eee7a094ae855fa6943
Sunset <s2821d3721@gmail.com>
2016-07-16 09:44:59 +0800

    Test: reporter blocks

--- hello.js
+++ hello.js
@@ -8,24 +8,18 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    // Functions for block with type 'w' will get a callback function as the 
-    // final argument. This should be called to indicate that the block can
-    // stop waiting.
-    ext.wait_random = function(callback) {
-        wait = Math.random();
-        console.log('Waiting for ' + wait + ' seconds');
-        window.setTimeout(function() {
-            callback();
-        }, wait*1000);
+    ext.power = function(base, exponent) {
+        return Math.pow(base, exponent);
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['w', 'wait for random time', 'wait_random'],
+            // Block type, block name, function name, param1 default value, param2 default value
+            ['r', '%n ^ %n', 'power', 2, 3],
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Random wait extension', descriptor, ext);
+    ScratchExtensions.register('Sample extension', descriptor, ext);
 })({});
