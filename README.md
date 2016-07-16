571d654b1e3d857912f756909850a80ec2ed5270
Sunset <s2821d3721@gmail.com>
2016-07-16 12:02:24 +0800

    Use gulp to build extension

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # ScratchX experiment
 
-Search ScratchX for course snp
+Search ScratchX for course snp.
 
+## Before start
+You need [nodejs](https://nodejs.org/en/) and [npm](https://www.npmjs.com/)(nodeJs package manager)  
+Install the package required to build, type `npm install --save` on command line.  
+If needed any other package, type `npm install --save packageYouWantToInstall`.  
+So that you can `var pkgName = require('my-pkg')` to include it.  
+
+## Build
+Extension source files are in `src/`  
+Type `gulp` or `gulp debug` to build. The final ScratchX extention will in `extensions/`
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+/*
+*	Compile from
+*     sources: src/*.js 
+*     to     : extensions/*.js
+*/
+
+// Import preprocessor
+var gulp       = require('gulp');
+var uglify     = require('gulp-uglify');
+var browserify = require('gulp-browserify');
+var babel      = require('gulp-babel');
+
+
+
+
+// Build instruction.
+// Type `gulp` in command line to trigger this task.
+gulp.task('default', function(){
+
+    process.env.NODE_ENV = 'production';
+    
+    return gulp.src('src/*.js')
+        .pipe(browserify())
+        .on('error', errorLog)
+        .pipe(babel({
+            presets: ['es2015'],
+            compact: true
+        }))
+        .on('error', errorLog)
+        .pipe(uglify())
+        .on('error', errorLog)
+        .pipe(gulp.dest('extensions/'));
+});
+
+
+
+
+// Build in debug mode
+// Type `gulp debug` in command line to trigger this task.
+gulp.task('debug', function(){
+    return gulp.src('src/*.js')
+        .pipe(browserify({
+            debug: true,
+        }))
+        .on('error', errorLog)
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .on('error', errorLog)
+        .pipe(gulp.dest('extensions/'));
+});
+
+
+
+
+// Define self used function 
+function errorLog(error) {
+    console.log(error);
+    console.error.bind(error);
+    this.emit('end');
+}
