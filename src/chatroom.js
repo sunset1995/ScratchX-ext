5509e1adb3e7bc8826e0365301407d6dec4051a3
Sunset <s2821d3721@gmail.com>
2016-07-16 16:17:20 +0800

    Add chatroom

---
+++ src/chatroom.js
@@ -0,0 +1,85 @@
+(function(ext) {
+
+
+
+    // Init socket
+    var io = require('socket.io-client')('http://snp2016.nctu.me:4444');
+    // Export to global
+    window.io = io;
+
+
+
+
+    // Message queue closure
+    var msgQueue = (function() {
+        // no queue in native js, use hash instead
+        var msgQueue = {};
+        var top = -1;
+        var tail = 0;
+        var size = 0;
+        return {
+            isEmpty: function() {
+                return top==tail-1;
+            },
+            push: function(text) {
+                ++size;
+                msgQueue[++top] = text;
+            },
+            pop: function() {
+                if( size==0 )
+                    return -1;
+                --size;
+                var ret = msgQueue[tail];
+                delete msgQueue[tail];
+                ++tail;
+                return ret;
+            },
+        }
+    })();
+
+    io.on('someOneSay', function(text) {
+        msgQueue.push(text);
+    });
+
+
+
+
+    // Send message to server
+    function say(text) {
+        io.emit('sayToAll', text);
+    };
+
+    // Whether there is message in queue
+    function msgQueueIsEmpty() {
+        return msgQueue.isEmpty();
+    }
+
+    // Get message from queue
+    function msgQueuePop(text) {
+        return msgQueue.pop();
+    }
+
+
+
+
+    // Scratch extentions
+    var ext = {
+        _shutdown: function() {},
+        _getStatus: function() {
+            return {status: 2, msg: 'Ready'};
+        },
+        say: say,
+    };
+
+    var descriptor = {
+        blocks: [
+            ['w', 'Say %s', 'say', "Hello, snp2016!"],
+            ['r', 'Message queue is empty', 'msgQueueIsEmpty'],
+            ['r', 'Message queue pop', 'msgQueuePop'],
+        ]
+    };
+
+    ScratchExtensions.register('Chatroom extension', descriptor, ext);
+
+
+})({});
