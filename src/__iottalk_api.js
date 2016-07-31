4f480c5dd6c8da7972ed61da1bbdea14621013cb
Sunset <s2821d3721@gmail.com>
2016-07-31 20:32:33 +0800

    Init scratchX project

---
+++ src/__iottalk_api.js
@@ -0,0 +1,114 @@
+function register(url, mac, profile, callback) {
+    $.ajax({
+        type: 'POST',
+        url: url + '/' + mac,
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({'profile': profile}),
+        success: function(res) {
+            console.log(res);
+            console.log('register success');
+        },
+        error: function(err, st) {
+            console.log(err);
+            console.log(st);
+            console.log('register failed');
+        },
+        complete: function() {
+            if( typeof callback === 'function' )
+                callback();
+        },
+        dataType: 'text',
+    });
+}
+
+
+function detach(url, mac, callback) {
+    $.ajax({
+        type: "DELETE",
+        url: url + '/' + mac,
+        success: function(res) {
+            console.log(res);
+            console.log('Detach success');
+        },
+        error: function(err, st) {
+            console.log(err);
+            console.log(st);
+            console.log('Detach failed');
+        },
+        complete: function() {
+            if( typeof callback === 'function' )
+                callback();
+        },
+        dataType: 'text',
+    });
+}
+
+
+function update(url, mac, feature, data, callback) {
+    $.ajax({
+        type: "PUT",
+        url: url + '/' + mac + '/' + feature,
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({'data': data}),
+        success: function(res) {
+            console.log(res);
+            console.log('Update success');
+        },
+        error: function(err, st) {
+            console.log(err);
+            console.log(st);
+            console.log('Update failed');
+        },
+        complete: function() {
+            if( typeof callback === 'function' )
+                callback();
+        },
+        dataType: 'text',
+    });
+}
+
+
+function get(url, mac, feature, callback) {
+    var ret = -1;
+    $.ajax({
+        type: "GET",
+        cache: false,
+        url: url + '/' + mac + '/' + feature,
+        success: function(res) {
+            ret = res;
+            console.log(res);
+            console.log('Get success');
+        },
+        error: function(err, st) {
+            console.log(err);
+            console.log(st);
+            console.log('Get failed');
+        },
+        complete: function() {
+            if( typeof callback === 'function' )
+                callback(ret);
+        },
+        dataType: 'text',
+    });   
+}
+
+
+
+
+// Export to browser's global for debug
+if( window ) {
+    window.IoTtalk = {
+        register: register,
+        detach: detach,
+        update: update,
+        get: get,
+    };
+}
+
+// Export api
+module.exports = {
+    register: register,
+    detach: detach,
+    update: update,
+    get: get,
+}
---
+++ src/iottalkDA-scratch.js
@@ -0,0 +1,92 @@
+(function(ext) {
+
+
+    // Include dependency
+    var api = require('__iottalk_api.js');
+    var SXregister = require('__scratchX-register.js');
+
+
+    // Define shared variable
+    var config = {
+        url: null,
+        mac: null,
+        registered: false,
+    };
+
+    var data = {};
+
+    const profile = {
+        'd_name': 'ScratchX',
+        'dm_name': 'ScratchX',
+        'is_sim': false,
+        'df_list': ['General_input', 'General_output']
+    };
+
+
+
+    // Implement scratchX extenion
+    function setServer(url) {
+        config.url = url;
+    }
+
+
+    function register(mac, callback) {
+        if( config.mac !== null ) {
+            callback();
+            return;
+        }
+
+        config.mac = mac;
+        var cb = function() {
+            config.registered = true;
+            callback();
+        };
+        api.register(config.url, mac, profile, cb);
+    }
+
+    function update(key, val, callback) {
+        data[key] = val;
+
+        if( config.url === null ) {
+            callback();
+            return;
+        }
+
+        const udp = JSON.stringify(data);
+        api.update(config.url, config.mac, 'General_input', udp, callback);
+    }
+
+    function get(mac, feature, key, callback) {
+        if( config.url === null ) {
+            callback();
+            return;
+        }
+
+        var cb = function(ret) {
+            if( typeof ret === 'object' )
+                callback(ret[key] || 0);
+            else
+                callback(ret);
+        };
+        api.get(config.url, mac, feature, cb);
+    }
+    
+
+
+
+    // Scratch extentions
+    SXregister.add(setServer, ' ', 'set remote server as %s', 'setServer', 'url');
+    SXregister.add(register, 'w', 'register mac address as %s', 'register', 'mac');
+    SXregister.add(update, 'w', 'update %s %s', 'update', 'key', 'val');
+    SXregister.add(get, 'R', 'get %s %s %s', 'get', 'mac', 'feature', 'key');
+
+
+
+
+    ScratchExtensions.register(
+        'Chatroom extension',
+        SXregister.descriptor,
+        SXregister.ext);
+
+
+})({});
