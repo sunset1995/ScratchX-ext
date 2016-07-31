81a46b48bf016be07160062a9f1e6dbc896c6fc6
Sunset <s2821d3721@gmail.com>
2016-07-31 21:38:32 +0800

    Debugging

--- src/iottalkDA-scratch.js
+++ src/iottalkDA-scratch.js
@@ -64,14 +64,12 @@
         }
 
         var cb = function(ret) {
-            if( typeof ret !== 'object' || 
-                    !ret['samples'] ||
-                    !ret['samples'][0] ) {
-                console.log(ret)
-                console.log(ret['samples']);
-                console.log(ret['samples'][0]);
+            console.log(typeof ret);
+            console.log(ret)
+            if( typeof ret !== 'object' )
                 callback(-1);
-            }
+            else if( !ret['samples'] || !ret['samples'][0] )
+                callback(-1);
             else {
                 console.log(ret['samples'][0][1][0]);
                 var res = JSON.parse(ret['samples'][0][1][0]);
